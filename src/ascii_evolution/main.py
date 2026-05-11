bf52b47eeaf537e6fa869775116616f423ccce74
HideOnHaru <heinert.nguyentien@gmail.com>
2026-05-11 13:49:32 +0200

    mutating und type hints

--- src/ascii_evolution/main.py
+++ src/ascii_evolution/main.py
@@ -4,7 +4,7 @@ import string
 
 target = "Evolutionary algorithms are fun!"
 population_size = 100
-mutation_rate = 0.01
+mutation_rate = 0.15
 generations = 1000
 
 def generate_random_string(length: int) -> str:
@@ -12,7 +12,7 @@ def generate_random_string(length: int) -> str:
     return "".join(random.choice(letters) for _ in range(length))
 
 
-def calculate_fitness(candidate, target):
+def calculate_fitness(candidate, target) -> int:
     f = 0
     for i in range(len(target)):
         f+= abs(ord(candidate[i]) - ord(target[i]))
@@ -23,19 +23,26 @@ def create_population(population_size, target_length):
     return [generate_random_string(target_length) for _ in range(population_size)]
 
 
-def tournament_selection(population):  
+def tournament_selection(population) -> str:  
     random_candidates = random.sample(population, 5)
     best_candidate = min(random_candidates, key=lambda c: calculate_fitness(c, target))
     return best_candidate
 
 
-def crossover(parent1, parent2):
+def crossover(parent1, parent2) -> str:
     crossover_point = random.randint(1, len(parent1) - 1)
     child = parent1[:crossover_point] + parent2[crossover_point:]
     return child
 
 
-
+def mutate_string(candidate) -> str:
+    new_candidate = ""
+    for char in candidate:
+        if random.random() < mutation_rate:
+            new_candidate += random.choice(string.ascii_letters + string.punctuation + ' ')
+        else:
+            new_candidate += char
+    return new_candidate
 
 
 
@@ -86,6 +93,15 @@ def main():
     print("Parent 2:", parent2)
     print("Child:   ", child)
 
+
+
+
+    mutated = mutate_string(target)
+
+    print("Original:", target)
+    print("Mutated: ", mutated)
+    print("Length OK:", len(target) == len(mutated))
+    print("Changed:", target != mutated)
 
 if __name__ == "__main__":
     main()
